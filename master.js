02a0d61e4bd6ef3f25684bc71a4f7e9a7e02a002
limelnoll <limelnoll@gmail.com>
2022-07-21 09:40:09 +0200

    Get the user input
    Finally, I could know how to get the user click

--- master.js
+++ master.js
@@ -1,17 +1,49 @@
+const btnOne = document.getElementById('one');
+const btnTwo = document.getElementById('two');
+const btnThree = document.getElementById('three');
+
 function computerPlay(){
   let x = parseInt(Math.random()*3);
   switch(x){
     case 0:
-      return 'Rock';
-    case 1:
       return 'Paper';
-    case 2: 
+    case 1:
+      return 'Rock';
+    case 2:
       return 'Scissor';
   }
 }
 
-let btn = document.getElementById('btn-one');
-btn.addEventListener('click', (e) => {
-  console.log(`You clicked ${e.target}`);
+btnOne.addEventListener('click', (e) => {
+  let com = computerPlay();
+  if (e.target.textContent === com){
+    console.log('You win');
+  } else {
+    divOne.textContent = `You Got ${e.target.textContent}, But The Computer Got ${com}`
+  }
 });
+
+btnTwo.addEventListener('click', (e) => {
+  let com = computerPlay();
+  if (e.target.textContent === com){
+    console.log('You win');
+  } else {
+    divOne.textContent = `You Got ${e.target.textContent}, But The Computer Got ${com}`
+  }
+});
+
+btnThree.addEventListener('click', (e) => {
+  let com = computerPlay();
+  if (e.target.textContent === com){
+    console.log('You win');
+  } else {
+    divOne.textContent = `You Got ${e.target.textContent}, But The Computer Got ${com}`
+  }
+});
+
+let myDiv = document.createElement('div');
+document.body.appendChild(myDiv);
+myDiv.setAttribute('id', 'myDivOne');
+let divOne = document.getElementById('myDivOne');
+
 
